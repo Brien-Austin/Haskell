6f8e073deed61cf3aa67cd0d925eba884dcc92b7
Brien-Austin <108070853+Brien-Austin@users.noreply.github.com>
2023-02-11 21:52:18 +0530

    Data type declaration

---
+++ definingdtypes.hs
@@ -0,0 +1,19 @@
+name :: String
+name = "Brien Austin"
+
+roll :: Int
+roll = 004
+
+grade :: Char
+grade = 'A'
+
+gpa :: Double
+gpa = 7.86
+
+main = do
+    print name
+    print roll
+    putChar  (grade)
+    putChar '\n'
+    print gpa
+  
